ebb350c7b0d0689546b9a206750b189282ac64bd
Angelo Loria <24941027+angelo-loria@users.noreply.github.com>
2024-03-05 14:40:34 +0000

    add index

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./pageObjects/base.page";
+export * from "./pageObjects/base.pageComponent";
+export * from "./pageObjects/components/footer";
+export * from "./pageObjects/components/navBar";
+export * from "./playwright.config";
